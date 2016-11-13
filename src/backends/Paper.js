1273a7aef5911c91377310c2f85ee5f3c2fe2cc8
Juan Cazala <cazala@mail.sfsu.edu>
2016-11-13 19:03:40 -0300

    Added functions to activate/propagate units individually

--- src/backends/Paper.js
+++ src/backends/Paper.js
@@ -10,63 +10,127 @@ export default class Paper {
     this.engine = engine || new Engine()
   }
 
+  activateUnit (unit, input) {
+    // glosary
+    const j = unit
+    const s = this.engine.state
+    const w = this.engine.weight
+    const g = this.engine.gain
+    const y = this.engine.activation
+    const f = this.activationFunction
+    const df = this.activationFunctionDerivative
+    const ε = this.engine.elegibilityTrace
+    const xε = this.engine.extendedElegibilityTrace
+
+    // unit sets
+    const inputSet = this.engine.inputSet
+    const gatedBy = this.engine.gatedBy
+    const inputsOfGatedBy = this.engine.inputsOfGatedBy
+
+    // this is only for input neurons (they receive their activation from the environment)
+    if (typeof input !== 'undefined') {
+
+      y[j] = input
+
+    } else {
+
+      // eq. 15
+      s[j] = g[j][j] * w[j][j] * s[j] + Σ(inputSet[j], i => g[j][i] * w[j][i] * y[i]) // compute state of j
+
+      // eq. 16
+      y[j] = f(j) // compute activation of j
+
+      for (const i of inputSet[j]) { // comupute elegibility traces for j's inputs
+
+        // eq. 17
+        ε[j][i] = g[j][j] * w[j][j] * ε[j][i] + g[j][i] * y[i]
+
+        for (const k of gatedBy[j]) { // compute extended elegibility traces for j's inputs
+
+          // eq. 18
+          xε[j][i][k] = g[k][k] * w[k][k] * xε[j][i][k] + df(j) * ε[j][i] * this.bigParenthesisTerm(k, j)
+        }
+      }
+
+      // update the gain of the connections gated by this unit with its activation value
+      for (const to of gatedBy[unit]) {
+        for (const from of inputsOfGatedBy[to][unit]) {
+          // eq. 14
+          g[to][from] = y[unit]
+        }
+      }
+    }
+
+    // return the activation of this unit
+    return y[j]
+  }
+
+  propagateUnit (unit, target) {
+    // glosary
+    const j = unit
+    const s = this.engine.state
+    const w = this.engine.weight
+    const g = this.engine.gain
+    const y = this.engine.activation
+    const df = this.activationFunctionDerivative
+    const δ = this.engine.errorResponsibility
+    const δP = this.engine.projectedErrorResponsibility
+    const δG = this.engine.gatedErrorResponsibility
+    const α = this.engine.learningRate
+    const ε = this.engine.elegibilityTrace
+    const xε = this.engine.extendedElegibilityTrace
+    const P = this.engine.projectionSet
+    const G = this.engine.gateSet
+
+    // unit sets
+    const inputSet = this.engine.inputSet
+
+    // step 1: compute error responsibiltity (δ) for j
+
+    if (typeof target !== 'undefined') { // this is only for output neurons, the error is injected from the environment
+
+      // eq. 10
+      δ[j] = δP[j] = target - y[j]
+
+    } else { // for the rest of the units the error is computed by backpropagation
+
+      // eq. 21
+      δP[j] = df(j) * Σ(P[j], k => δ[k] * g[k][j] * w[k][j])
+
+      // eq. 22
+      δG[j] = df(j) * Σ(G[j], k => δ[k] * this.bigParenthesisTerm(k, j))
+
+      // eq. 23
+      δ[j] = δP[j] + δG[j]
+
+    }
+
+    // step 2: adjust the weights (Δw) for all the inputs of j
+
+    for (const i of inputSet[j]) {
+      // eq. 24
+      w[j][i] += α * δP[j] * ε[j][i] + α * Σ(G[j], k => δ[k] * xε[j][i][k])
+    }
+  }
+
+  // this calculate the big parenthesis term that is present in eq. 18 and eq. 22
+  bigParenthesisTerm (k, j) {
+    // glosary
+    const w = this.engine.weight
+    const s = this.engine.state
+    const y = this.engine.activation
+    const dt = this.engine.derivativeTerm[k][j] // the derivative term is 1 if and only if j gates k's self-connection, otherwise is 0
+    const gatedInputs = this.engine.inputsOfGatedBy[k][j] // this index runs over all the inputs of k, that are gated by j
+
+    // big parenthesis term
+    return dt * w[k][k] * s[k] + Σ(gatedInputs, a => w[k][a] * y[a])
+  },
+
   activate (inputs) {
     this.engine.status = StatusTypes.ACTIVATING
     const activations = this.engine.layers.map((layer, layerIndex) => {
       return layer.map((unit, unitIndex) => {
-
-        // glosary
-        const j = unit
-        const s = this.engine.state
-        const w = this.engine.weight
-        const g = this.engine.gain
-        const y = this.engine.activation
-        const f = this.activationFunction
-        const df = this.activationFunctionDerivative
-        const ε = this.engine.elegibilityTrace
-        const xε = this.engine.extendedElegibilityTrace
-
-        // unit sets
-        const inputSet = this.engine.inputSet
-        const gatedBy = this.engine.gatedBy
-        const inputsOfGatedBy = this.engine.inputsOfGatedBy
-
-        // this is only for input neurons (they receive their activation from the environment)
-        if (layerIndex === 0) {
-
-          y[j] = inputs[unitIndex]
-
-        } else {
-
-          // eq. 15
-          s[j] = g[j][j] * w[j][j] * s[j] + Σ(inputSet[j], i => g[j][i] * w[j][i] * y[i]) // compute state of j
-
-          // eq. 16
-          y[j] = f(j) // compute activation of j
-
-          for (const i of inputSet[j]) { // comupute elegibility traces for j's inputs
-
-            // eq. 17
-            ε[j][i] = g[j][j] * w[j][j] * ε[j][i] + g[j][i] * y[i]
-
-            for (const k of gatedBy[j]) { // compute extended elegibility traces for j's inputs
-
-              // eq. 18
-              xε[j][i][k] = g[k][k] * w[k][k] * xε[j][i][k] + df(j) * ε[j][i] * this.bigParenthesisTerm(k, j)
-            }
-          }
-
-          // update the gain of the connections gated by this unit with its activation value
-          for (const to of gatedBy[unit]) {
-            for (const from of inputsOfGatedBy[to][unit]) {
-              // eq. 14
-              g[to][from] = y[unit]
-            }
-          }
-        }
-
-        // return the activation of this unit
-        return y[j]
+        this.activateUnit(unit, layerIndex === 0 ? inputs[unitIndex] : void 0)
       })
     })
     this.engine.status = StatusTypes.IDLE
@@ -81,55 +145,7 @@ export default class Paper {
     .forEach((layer, layerIndex) => {
       layer.slice().reverse() // units get propagated in reverse order
       .forEach((unit, unitIndex) => {
-
-        // glosary
-        const j = unit
-        const s = this.engine.state
-        const w = this.engine.weight
-        const g = this.engine.gain
-        const y = this.engine.activation
-        const df = this.activationFunctionDerivative
-        const δ = this.engine.errorResponsibility
-        const δP = this.engine.projectedErrorResponsibility
-        const δG = this.engine.gatedErrorResponsibility
-        const α = this.engine.learningRate
-        const ε = this.engine.elegibilityTrace
-        const xε = this.engine.extendedElegibilityTrace
-        const P = this.engine.projectionSet
-        const G = this.engine.gateSet
-
-        // unit sets
-        const inputSet = this.engine.inputSet
-
-        // target value
-        const target = targets[index]
-
-        // step 1: compute error responsibiltity (δ) for j
-
-        if (typeof target !== 'undefined') { // this is only for output neurons, the error is injected from the environment
-
-          // eq. 10
-          δ[j] = δP[j] = target - y[j]
-
-        } else { // for the rest of the units the error is computed by backpropagation
-
-          // eq. 21
-          δP[j] = df(j) * Σ(P[j], k => δ[k] * g[k][j] * w[k][j])
-
-          // eq. 22
-          δG[j] = df(j) * Σ(G[j], k => δ[k] * this.bigParenthesisTerm(k, j))
-
-          // eq. 23
-          δ[j] = δP[j] + δG[j]
-
-        }
-
-        // step 2: adjust the weights (Δw) for all the inputs of j
-
-        for (const i of inputSet[j]) {
-          // eq. 24
-          w[j][i] += α * δP[j] * ε[j][i] + α * Σ(G[j], k => δ[k] * xε[j][i][k])
-        }
+        this.activateUnit(unit, layerIndex === 0 ? targets[unitIndex] : void 0)
       })
     })
     this.engine.status = StatusTypes.IDLE
@@ -166,19 +182,6 @@ export default class Paper {
       })
     })
   }
-
-  // this calculate the big parenthesis term that is present in eq. 18 and eq. 22
-  bigParenthesisTerm (k, j) {
-    // glosary
-    const w = this.engine.weight
-    const s = this.engine.state
-    const y = this.engine.activation
-    const dt = this.engine.derivativeTerm[k][j] // the derivative term is 1 if and only if j gates k's self-connection, otherwise is 0
-    const gatedInputs = this.engine.inputsOfGatedBy[k][j] // this index runs over all the inputs of k, that are gated by j
-
-    // big parenthesis term
-    return dt * w[k][k] * s[k] + Σ(gatedInputs, a => w[k][a] * y[a])
-  },
 
   activationFunction (unit) {
     let x
