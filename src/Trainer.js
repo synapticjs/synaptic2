20d8535d067871f7f6ef0df0b4cd549c61a89dc1
Juan Cazala <cazala@mail.sfsu.edu>
2016-11-13 01:20:13 -0300

    Added basic trainer

--- src/Trainer.js
+++ src/Trainer.js
@@ -1,8 +1,21 @@
 
-// -- cost types
+// -- Cost Types
 
-export CostTypes = {
+export const CostTypes = {
   CROSS_ENTROPY: 'Cross Entropy',
   MSE: 'Mean Square Error',
   BINARY: 'Binary'
+}
+
+// -- Trainer
+
+export default class Trainer {
+
+    constructor (network) {
+      this.network = network
+    }
+
+    train (dataset, options) {
+      return network.backend.train(dataset, options)
+    }
 }
