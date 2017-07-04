ec3dd4579c6458cd35ccd77cf8bb2411aee38a62
Juan Cazala <cazala@mail.sfsu.edu>
2017-07-03 22:54:10 -0300

    added coveralls/

--- test/performance.spec.ts
+++ test/performance.spec.ts
@@ -7,59 +7,59 @@ import { run as notMochaRunner, storeResults } from './performance/performanceRu
 
 function run(test: string, backend, only?: boolean) {
 
-  const fn = only ? it.only : it;
+    const fn = only ? it.only : it;
 
-  fn(test, function (done) {
-    this.timeout(360000);
+    fn(test, function(done) {
+        this.timeout(360000);
 
-    notMochaRunner(test, { backend }).then(() => done(), err => done(err));
-  });
+        notMochaRunner(test, { backend }).then(() => done(), err => done(err));
+    });
 }
 
 
 describe('Performance tasks', () => {
-  // describe('Paper', () => {
-  //   it('XOR', run('XOR', backends.Paper));
-  //   it('AND', run('AND', backends.Paper));
-  //   it('OR', run('OR', backends.Paper));
-  //   it('NOT', run('NOT', backends.Paper));
-  // });
+    // describe('Paper', () => {
+    //   it('XOR', run('XOR', backends.Paper));
+    //   it('AND', run('AND', backends.Paper));
+    //   it('OR', run('OR', backends.Paper));
+    //   it('NOT', run('NOT', backends.Paper));
+    // });
 
-  // describe('CPU', () => {
-  //   it('XOR', run('XOR', backends.CPU));
-  //   it('AND', run('AND', backends.CPU));
-  //   it('OR', run('OR', backends.CPU));
-  //   it('NOT', run('NOT', backends.CPU));
-  // });
+    // describe('CPU', () => {
+    //   it('XOR', run('XOR', backends.CPU));
+    //   it('AND', run('AND', backends.CPU));
+    //   it('OR', run('OR', backends.CPU));
+    //   it('NOT', run('NOT', backends.CPU));
+    // });
 
-  describe('ASM', () => {
-    run('XOR', backends.ASM);
-    run('AND', backends.ASM);
-    run('OR', backends.ASM);
-    run('NOT', backends.ASM);
-    run('TIMING_TASK', backends.ASM, true);
-    run('DSR', backends.ASM);
-    run('MNIST', backends.ASM);
-    run('CONV_MNIST', backends.ASM);
-    run('SOFTMAX_MNIST', backends.ASM);
-    // run('WRITE', backends.ASM);
-  });
+    describe('ASM', () => {
+        run('XOR', backends.ASM);
+        run('AND', backends.ASM);
+        run('OR', backends.ASM);
+        run('NOT', backends.ASM);
+        run('TIMING_TASK', backends.ASM);
+        run('DSR', backends.ASM);
+        run('MNIST', backends.ASM);
+        run('CONV_MNIST', backends.ASM);
+        run('SOFTMAX_MNIST', backends.ASM);
+        // run('WRITE', backends.ASM);
+    });
 
-  describe('WASM', () => {
-    run('XOR', backends.WASM);
-    run('TIMING_TASK', backends.WASM);
-    run('DSR', backends.WASM);
-    // run('MNIST', backends.WASM);
-    // run('SOFTMAX_MNIST', backends.WASM);
-  });
+    describe('WASM', () => {
+        run('XOR', backends.WASM);
+        run('TIMING_TASK', backends.WASM);
+        run('DSR', backends.WASM);
+        // run('MNIST', backends.WASM);
+        // run('SOFTMAX_MNIST', backends.WASM);
+    });
 });
 
 
-after(function () {
-  this.timeout(10000);
-  if (process.env.CI) { // do not store results in CI
-    console.log('Skipping storeResults in CI');
-  } else {
-    storeResults('test-results');
-  }
+after(function() {
+    this.timeout(10000);
+    if (process.env.CI) { // do not store results in CI
+        console.log('Skipping storeResults in CI');
+    } else {
+        storeResults('test-results');
+    }
 });
