65d8329be7593928d2f5ea40bd175e28991cedea
Juan Cazala <cazala@mail.sfsu.edu>
2017-07-09 21:09:40 -0300

    test

--- test/performance/specs/CONV_MNIST.ts
+++ test/performance/specs/CONV_MNIST.ts
@@ -5,7 +5,6 @@ import mnist = require('mnist');
 import { layers, Network, CostTypes } from '../../../src';
 import { PerformanceTest } from "../interfaces";
 import { TrainEntry, TrainResult } from "../../../src/backends/index";
-import { Activations } from "lysergic";
 import { logTopology } from "../../../src/utils/topologyPrinter";
 
 const generator = new MersenneTwister(100010);
@@ -15,68 +14,94 @@ let mnistSet: { training: TrainEntry[], test: TrainEntry[] } = { training: [], t
 
 
 { // initialize training set
-  const oldRandom = Math.random;
-  Math.random = random;
-  try {
-    mnistSet = mnist.set(5000, 1000);
-  } finally {
-    Math.random = oldRandom;
-  }
+    const oldRandom = Math.random;
+    Math.random = random;
+    try {
+        mnistSet = mnist.set(5000, 1000);
+    } finally {
+        Math.random = oldRandom;
+    }
 }
-Activations
+
 let baseNetwork = new Network({
-  generator: random,
-  layers: [
-    new layers.Input2D(28, 28),
-    new layers.Convolution2D({
-      padding: 3,
-      stride: 2,
-      filter: 5,
-      depth: 2
-    }),
-    new layers.Softmax(10)
-  ],
-  engineOptions: {
-    bias: false
-  }
+    generator: random,
+    layers: [
+        new layers.Input2D(28, 28),
+        new layers.Convolution2D({
+            padding: 3, // valid/same default same
+            stride: 2, // strides
+            filter: 5, // kernel_size
+            depth: 1 // filters
+        }),
+        // new layers.MaxPool2D(2),
+        new layers.Softmax(10)
+    ],
+    engineOptions: {
+        bias: false
+    }
 });
+/*
+let baseNetwork = new Network({
+    generator: random,
+    layers: [
+        new layers.Input2D(2, 2),
+        new layers.MaxPool2D(2)
+    ],
+    engineOptions: {
+        bias: false
+    }
+});
+*/
+
 
 console.log('CONV_MNIST Topology: \n' + logTopology(baseNetwork));
 
 export class CONV_MNIST extends PerformanceTest {
-  costFunction: CostTypes = CostTypes.SOFTMAX;
-  logEvery = 1;
-  maxIterations = 100;
-  minError = 0.05;
-  learningRate = 0.5;
+    costFunction: CostTypes = CostTypes.SOFTMAX;
+    logEvery = 1;
+    maxIterations = 100;
+    minError = 0.05;
+    learningRate = 0.5;
 
-  async build(backend) {
+    async build(backend) {
 
-    const network = baseNetwork.clone();
-    network.backend = new backend(network.compiler);
-    await network.build();
+        const network = baseNetwork.clone();
+        network.backend = new backend(network.compiler);
+        await network.build();
 
-    return network;
-  }
+        return network;
+    }
 
-  log(partial: TrainResult, errorSet: ArrayLike<number>, network: Network) {
-    super.log(partial, errorSet, network);
-    // network.compiler.learningRate = partial.error * 0.5; // * 0.1;
-  }
+    log(partial: TrainResult, errorSet: ArrayLike<number>, network: Network) {
+        super.log(partial, errorSet, network);
+        // network.compiler.learningRate = partial.error * 0.5; // * 0.1;
+    }
 
-  async getTrainigSet() {
-    return mnistSet.training.map($ => ({
-      input: $.input.map($ => $ - 0.5),
-      output: $.output
-    }));
-  }
+    async getTrainigSet() {
+        /*return [
+            {
+                input: [0, 0, 0, 0],
+                output: [0]
+            }
+        ];*/
+        return mnistSet.training.map($ => ({
+            input: $.input.map($ => $ - 0.5),
+            output: $.output
+        }));
+    }
 
-  async getTestingSet() {
-    return mnistSet.test.map($ => ({
-      input: $.input.map($ => $ - 0.5),
-      output: $.output
-    }));
-  }
+    async getTestingSet() {
+        /*return [
+            {
+                input: [0, 0, 0, 0],
+                output: [0]
+            }
+        ]*/
+        return mnistSet.test.map($ => ({
+            input: $.input.map($ => $ - 0.5),
+            output: $.output
+        }));
+    }
 };
 
 export default new CONV_MNIST;
