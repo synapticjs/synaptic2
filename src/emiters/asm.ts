b5011444288b9e629e73ea4149d33c8ea4238f24
menduz <agustin.mz.92@gmail.com>
2017-07-12 20:16:26 -0300

    Add for loop to asm emiter

--- src/emiters/asm.ts
+++ src/emiters/asm.ts
@@ -41,6 +41,8 @@ return {
         lhsString = `H[${node.lhs.position}]` + onlyWhenDebugging(`/* ${node.lhs.key} */`);
       } else if (node.lhs instanceof nodes.HeapReferenceNode) {
         lhsString = `H[${node.lhs.position}]`;
+      } else if (node.lhs instanceof nodes.HeapPointer) {
+        lhsString = `H[(${emit(node.lhs.position)} | 0)]`;
       }
     }
 
@@ -59,14 +61,16 @@ return {
     if (node.hasParenthesis)
       return `+H[${node.position}]` + onlyWhenDebugging(`/* ${node.key} */`);
     else
-      return `(+H[${node.position}])` + onlyWhenDebugging(`/* ${node.key} */)`);
+      return `(+H[${node.position}]` + onlyWhenDebugging(`/* ${node.key} */`) + ')';
   } else if (node instanceof nodes.HeapReferenceNode) {
     if (node.hasParenthesis)
       return `+H[${node.position}]`;
     else
       return `(+H[${node.position}])`;
   } else if (node instanceof nodes.FloatNumberNode) {
-    return node.numericValue.toFixed(1);
+    return node.numericValue.toFixed(10);
+  } else if (node instanceof nodes.IntNumberNode) {
+    return node.numericValue.toFixed(0);
   } else if (node instanceof nodes.TernaryExpressionNode) {
     return '((' + emit(node.condition) + ') ? (' + emit(node.truePart) + ') : (' + emit(node.falsePart) + '))';
   } else if (node instanceof nodes.UnaryExpressionNode) {
@@ -75,18 +79,42 @@ return {
     return (
       onlyWhenDebugging('/* ' + (node.name || 'Unnamed block') + ' */\n')
       +
-      indent(node.children.map(x => emit(x) + ';').join('\n')) + '\n'
+      indent(node.children.map(x => emit(x) + ';').join('\n')) + onlyWhenDebugging('\n')
     );
 
   } else if (node instanceof nodes.FunctionNode) {
     return `function ${node.name}() {
   ${indent(emit(node.body))}
 }`;
+  } else if (node instanceof nodes.ForLoopNode) {
+    const varName = node.var.variable.name;
+
+    return [
+      `for(${varName} = ${node.from.toFixed(0)}; (${varName}|0) < ${node.to.toFixed(0)}; ${varName} = (${varName}+1)|0) {`,
+      indent(emit(node.expression)),
+      `}`
+    ].join('\n');
+  } else if (node instanceof nodes.VariableReference) {
+    if (node.variable.type == 'int')
+      return `(${node.variable.name} | 0)`;
+    else
+      return `(+${node.variable.name})`;
+  } else if (node instanceof nodes.VariableDeclaration) {
+    if (node.type == 'int')
+      return `var ${node.name} = ${node.initialValue.toFixed(0)};`;
+    else
+      return `var ${node.name} = ${node.initialValue.toFixed(10)};`;
+  } else if (node instanceof nodes.HeapPointer) {
+    if (node.hasParenthesis)
+      return `+H[(${emit(node.position)} | 0)]`;
+    else
+      return `(+H[(${emit(node.position)} | 0)])`;
   }
   throw new Error('CANNOT PRINT NODE: ' + node.constructor.toString());
 };
 
 export function emit(node: nodes.Node) {
+  if (!node) return '!!!NULL!!!';
   if (node.hasParenthesis)
     return '(' + baseEmit.call(null, node) + ')';
   return baseEmit.call(null, node);
